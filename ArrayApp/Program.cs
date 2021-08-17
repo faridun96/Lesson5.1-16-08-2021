79e93152d63dc2f6969e814ea1f7e0efea13b40b
faridun96 <faridun.nk@gmail.com>
2021-08-17 10:59:53 +0500

    ArrayApp

---
+++ ArrayApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов");
+            int num = int.Parse(Console.ReadLine());
+            int[] a = new int[num];
+            Console.WriteLine("Введите элементы");
+            for (int i = 0; i < num; i++) 
+            {
+                Console.Write("array[{0}] = ",i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Элементы в обратном порядке");
+            for (int i = num - 1; i >= 0; i--) {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
